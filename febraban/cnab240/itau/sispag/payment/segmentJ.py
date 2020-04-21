cded1ecd980400b7208a72ff0cf521bef3d4410d
ph_santosjr <paulo.henrique@databrain.com.br>
2020-04-21 11:23:12 -0300

    format barcode segment J

--- febraban/cnab240/itau/sispag/payment/segmentJ.py
+++ febraban/cnab240/itau/sispag/payment/segmentJ.py
@@ -30,7 +30,7 @@ class SegmentJ:
             difference_caracters = 47 - len(barCode)
             barCode = barCode + ('0' * difference_caracters)
 
-        barCode = barCode[0:4] + barCode[32:49] + barCode[4:10] + barCode[11:21] + barCode[21:31]
+        barCode = barCode[0:4] + barCode[32:49] + barCode[4:9] + barCode[10:21] + barCode[21:31]
 
         structs = [
             (17,  61,  44, numeric, barCode)
