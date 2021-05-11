021a62d43b30bb213cc741f91be414146633a62c
Adriano Margarin <adriano.margarin@gmail.com>
2021-05-11 14:32:15 -0300

    removido clean_data_return_only_alphanumeric

--- febraban/cnab240/user.py
+++ febraban/cnab240/user.py
@@ -1,10 +1,12 @@
-from .libs.cleanData import clean_data_return_only_number, clean_data_return_only_alphanumeric
+from .libs.cleanData import (
+    clean_data_return_only_number,
+    clean_data_return_only_alphanumeric,
+)
 
 
 class User:
-
     def __init__(self, name, identifier, bank=None, address=None, file_id=None):
-        self.name = clean_data_return_only_alphanumeric(name)
+        self.name = name
         self.identifier = clean_data_return_only_number(identifier)
         self.bank = bank
         self.address = address
@@ -12,19 +14,21 @@ class User:
 
 
 class UserBank:
-
-    def __init__(self, bankId, branchCode, accountNumber, accountVerifier, bankName="", code=""):
+    def __init__(
+        self, bankId, branchCode, accountNumber, accountVerifier, bankName="", code=""
+    ):
         self.bankId = bankId
         self.bankName = bankName
         self.accountNumber = accountNumber
         self.branchCode = branchCode
         self.accountVerifier = accountVerifier
-        self.code = code        # código do convênio com o banco
+        self.code = code  # código do convênio com o banco
 
 
 class UserAddress:
-
-    def __init__(self, streetLine1, city, stateCode, zipCode, district="", streetLine2=""):
+    def __init__(
+        self, streetLine1, city, stateCode, zipCode, district="", streetLine2=""
+    ):
         self.streetLine1 = streetLine1
         self.streetLine2 = streetLine2
         self.district = district
